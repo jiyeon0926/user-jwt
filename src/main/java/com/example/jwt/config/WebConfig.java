9a47fffd4f61a71f9af0ed9658c692b83b53fa1a
jiyeon0926 <yeoni9094@naver.com>
2025-03-03 23:50:10 +0900

    feat: SecurityFilterChain
    - 화이트 리스트에 회원가입 경로 추가
    - 화이트 리스트에 속한 경로는 인증 없이 접근 가능
    - 이외 경로는 인증 필요

---
+++ src/main/java/com/example/jwt/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.example.jwt.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig {
+
+    private static final String[] WHITE_LIST = {"/users/signup"};
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(WHITE_LIST).permitAll()
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE,
+                                        DispatcherType.ERROR).permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
