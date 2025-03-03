4f4369397986d486d0ae75de7b7328f4fce05c0f
jiyeon0926 <yeoni9094@naver.com>
2025-03-04 05:37:03 +0900

    fix: JWT 및 인증/인가 Exception 추가
    - AuthenticationException
    - AccessDeniedException
    - AuthorizationDeniedException
    - JwtException

--- src/main/java/com/example/jwt/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/jwt/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,13 @@
 package com.example.jwt.common.exception;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -54,5 +60,49 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getStatusCode())
                 .body(e.getReason());
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    protected ResponseEntity<String> handleAuthException(
+            AuthenticationException e) {
+        HttpStatus statusCode = e instanceof BadCredentialsException
+                ? HttpStatus.FORBIDDEN
+                : HttpStatus.UNAUTHORIZED;
+
+        return ResponseEntity
+                .status(statusCode)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    protected ResponseEntity<String> handleAccessDeniedException(AccessDeniedException e) {
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    protected ResponseEntity<String> handleAuthorizationDeniedException(AuthorizationDeniedException e) {
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(JwtException.class)
+    protected ResponseEntity<String> handleJwtException(JwtException e) {
+        HttpStatus httpStatus = e instanceof ExpiredJwtException ? HttpStatus.UNAUTHORIZED : HttpStatus.FORBIDDEN;
+
+        return ResponseEntity
+                .status(httpStatus)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<String> handleOtherExceptions(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(e.getMessage());
     }
 }
