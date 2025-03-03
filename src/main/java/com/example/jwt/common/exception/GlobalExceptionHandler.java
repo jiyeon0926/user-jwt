34fd642aab989f258a959b276b75533722e9abf6
jiyeon0926 <yeoni9094@naver.com>
2025-03-04 00:32:08 +0900

    feat: GlobalExceptionHandler
    - ValidationException
    - IllegalArgumentException
    - ResponseStatusException

---
+++ src/main/java/com/example/jwt/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.example.jwt.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    protected ResponseEntity<String> handleMethodValidationExceptions(
+            HandlerMethodValidationException e) {
+        String message = e.getParameterValidationResults()
+                .getFirst()
+                .getResolvableErrors()
+                .getFirst()
+                .getDefaultMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(message);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(
+            MethodArgumentNotValidException e) {
+        String message = e.getBindingResult()
+                .getAllErrors()
+                .getFirst()
+                .getDefaultMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(message);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentExceptions(
+            IllegalArgumentException e) {
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    protected ResponseEntity<String> handleResponseStatusExceptions(
+            ResponseStatusException e) {
+
+        return ResponseEntity
+                .status(e.getStatusCode())
+                .body(e.getReason());
+    }
+}
